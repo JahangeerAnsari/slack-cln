77bb5ad9f7fb66243e94c09529374705846cfeb1
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-10-16 13:29:37 +0530

    hint component added for sidebar header

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,19 +1,34 @@
 "use client";
 import { Sidebar } from "./components/sidebar";
-import { Toolbar } from "./components/toolbar"
-
-interface WorkspaceIdPageLayoutProps{
-    children:React.ReactNode
+import { Toolbar } from "./components/toolbar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { WorkspaceSidebar } from "./components/workspace-sidebar";
+interface WorkspaceIdPageLayoutProps {
+  children: React.ReactNode;
 }
-const WorkspaceIdPageLayout = ({children}:WorkspaceIdPageLayoutProps) =>{
-    return(
-        <div className="h-full overflow-hidden">
-            <Toolbar/>
-            <div className="flex h-[calc(100vh-40px)]">
-             <Sidebar/>
-            </div>
-            {children}
-        </div>
-    )
-}
-export default WorkspaceIdPageLayout
+const WorkspaceIdPageLayout = ({ children }: WorkspaceIdPageLayoutProps) => {
+  return (
+    <div className="h-full overflow-hidden">
+      <Toolbar />
+      <div className="flex h-[calc(100vh-40px)]">
+        <Sidebar />
+        <ResizablePanelGroup direction="horizontal" autoSaveId ="ca-workspace-layout">
+            <ResizablePanel defaultSize={15} minSize={11} className="bg-[#5E2C5F]">
+              {/* WORKSPACE SIDEBAR */}
+              <WorkspaceSidebar/>
+            </ResizablePanel>
+            <ResizableHandle withHandle/>
+            <ResizablePanel>
+                 {children}
+            </ResizablePanel>
+        </ResizablePanelGroup>
+       
+      </div>
+    </div>
+  );
+};
+export default WorkspaceIdPageLayout;
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -4,6 +4,7 @@ const WorkspaceIdPage = () =>{
    
     return (
         <div>
+            Workspace Id Page
         </div>
     )
 }
