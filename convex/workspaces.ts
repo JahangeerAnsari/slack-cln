d254b07bdd73b1a52077d39f5cdbabe4fb2ddfc9
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-10-18 19:32:54 +0530

    update and delete workspace functionality added.

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -51,6 +51,59 @@ export const getById = query({
     return await ctx.db.get(args.id)
   }
 })
+// update workspace
+export const update = mutation({
+  args:{
+    id:v.id("workspaces"),
+    name:v.string()
+  },
+  handler:async(ctx,args)=>{
+    const userId = await auth.getUserId(ctx);
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+     // only fetched workspace which is i am the part of the workspace
+    const member =  await ctx.db.query("members")
+    .withIndex("by_workspace_id_user_id",(q) => q.eq("workspaceId",args.id)
+    .eq("userId",userId)).unique();
+    if(!member || member.role !=="admin"){
+        throw new Error("Unauthorized");
+    }
+  await ctx.db.patch(args.id,{
+      name:args.name
+    });
+    return args.id
+  }
+})
+// delete workspace
+export const remove = mutation({
+  args:{
+    id:v.id("workspaces"),
+  },
+  handler:async(ctx,args)=>{
+    const userId = await auth.getUserId(ctx);
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+     // only fetched workspace which is i am the part of the workspace
+    const member =  await ctx.db.query("members")
+    .withIndex("by_workspace_id_user_id",(q) => q.eq("workspaceId",args.id)
+    .eq("userId",userId)).unique();
+    if(!member || member.role !=="admin"){
+        throw new Error("Unauthorized");
+    }
+    // delete also member which is associated with workspaces
+    const[members] = await Promise.all([
+      ctx.db.query("members")
+      .withIndex("by_workspace_id",(q) => q.eq("workspaceId",args.id)).collect()
+    ])
+    for(const member of members){
+      await ctx.db.delete(member._id)
+    }
+  await ctx.db.delete(args.id)
+    return args.id
+  }
+})
 export const get = query({
   args: {},
   handler: async (ctx) => {
