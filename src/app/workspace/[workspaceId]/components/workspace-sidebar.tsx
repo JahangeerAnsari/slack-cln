7c2ce4f3e6b83af25b4a6857897de5a9c553c1df
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-11-28 18:16:40 +0530

    addes user-item component to show user details and it redirect to direct msg to the user

--- src/app/workspace/[workspaceId]/components/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/components/workspace-sidebar.tsx
@@ -1,22 +1,36 @@
-import { useGetCurrentMember } from "@/features/members/api/use-get-current";
+import { useGetCurrentMember } from "@/features/members/api/use-get-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { AlertTriangle, HashIcon, Loader, MessageSquareText, SendHorizonal } from "lucide-react";
+import {
+  AlertTriangle,
+  HashIcon,
+  Loader,
+  MessageSquareText,
+  SendHorizonal,
+} from "lucide-react";
 import { WorkspaceHeader } from "./workspace-header";
 import { SidebarItem } from "./sidebar-item";
 import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { WorkspaceSection } from "./workspace-section";
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { UserItem } from "./user-item";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
-  const { data: member, isLoading: isMemberLoading } = useGetCurrentMember({
-    workspaceId,
-  });
+  const { data: member, isLoading: isCurrentMemberLoading } =
+    useGetCurrentMember({
+      workspaceId,
+    });
   const { data: workspace, isLoading: isWorkspaceLoading } = useGetWorkspace({
     id: workspaceId,
   });
-  const {data:channels,isLoading:isChannelLoading} =  useGetChannels({workspaceId})
-  if (isWorkspaceLoading || isMemberLoading) {
+  const { data: channels, isLoading: isChannelLoading } = useGetChannels({
+    workspaceId,
+  });
+  const { data: members, isLoading: isMembersLoading } = useGetMembers({
+    workspaceId,
+  });
+  if (isWorkspaceLoading || isCurrentMemberLoading) {
     return (
       <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
         <Loader className="size-5 animate-spin text-white" />
@@ -38,31 +52,31 @@ export const WorkspaceSidebar = () => {
         isAdmin={member.role === "admin"}
       />
       <div className="flex flex-col px-2 mt-3">
-        <SidebarItem
-          label="Threads"
-          icon={MessageSquareText}
-          id="threads"
-          
-        />
-        <SidebarItem
-          label="Drafts & Send"
-          icon={SendHorizonal}
-          id="drafts"
-        />
-          </div>
-        <WorkspaceSection label="Channels" hint="New Channel"
-         onNew={() => {}}
-        >
-          {channels?.map((item) => (
+        <SidebarItem label="Threads" icon={MessageSquareText} id="threads" />
+        <SidebarItem label="Drafts & Send" icon={SendHorizonal} id="drafts" />
+      </div>
+      <WorkspaceSection label="Channels" hint="New Channel" onNew={() => {}}>
+        {channels?.map((item) => (
           <SidebarItem
-          key={item._id}
-          icon={HashIcon}
-          label={item.name}
-          id={item._id}
+            key={item._id}
+            icon={HashIcon}
+            label={item.name}
+            id={item._id}
           />
         ))}
-        </WorkspaceSection>
-    
+      </WorkspaceSection>
+      <WorkspaceSection label="Direct Messages" hint="New Direct Message" 
+      onNew={() =>{}}
+      >
+        {members?.map((item) => (
+          <UserItem
+            key={item._id}
+            id={item._id}
+            image={item?.user.image}
+            label={item.user.name}
+          />
+        ))}
+      </WorkspaceSection>
     </div>
   );
 };
