a5eb906b3d57d52cf922c609d2673a252c6f0672
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-12-20 15:05:40 +0530

    redirect to the channelId url and if there is no channel open the channel modal

---
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -0,0 +1,9 @@
+const ChannelIdPage = () => {
+    return (
+        <div>
+         <h1>channel id page</h1>
+        </div>
+      );
+}
+ 
+export default ChannelIdPage;
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,11 +1,42 @@
 "use client";
 
-const WorkspaceIdPage = () =>{
-   
-    return (
-        <div>
-            Workspace Id Page
-        </div>
-    )
-}
-export default WorkspaceIdPage
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useChannelStore } from "@/features/channels/store/use-channel-store";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
+import { useEffect, useMemo } from "react";
+
+const WorkspaceIdPage = () => {
+  const { onOpen } = useChannelStore();
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+  const { data: channels, isLoading: channelLoading } = useGetChannels({
+    workspaceId,
+  });
+  const channelId = useMemo(() => channels?.[0]._id, [channels]);
+  useEffect(() => {
+    if (workspaceLoading || channelLoading || !workspace) {
+      return;
+    }
+    if (channelId) {
+      router.push(`/workspace/${workspaceId}/channel/${channelId}`);
+    } else if (!channelId) {
+      onOpen("createChannel");
+    }
+  }, [
+    channelId,
+    channelLoading,
+    onOpen,
+    router,
+    workspace,
+    workspaceId,
+    workspaceLoading,
+  ]);
+
+  return <div>Workspace Id Page</div>;
+};
+export default WorkspaceIdPage;
