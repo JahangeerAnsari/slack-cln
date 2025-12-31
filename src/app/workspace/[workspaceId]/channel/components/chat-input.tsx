bb1f6ded9c3d9f2b5ec081b658025e67a57b4671
JahangeerAnsari <jahangeeransari194@gmail.com>
2025-12-31 13:05:12 +0530

    adding try catch to the submit create message function

--- src/app/workspace/[workspaceId]/channel/components/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/components/chat-input.tsx
@@ -5,7 +5,8 @@ import { useChannelId } from "@/hooks/use-channel-id";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import dynamic from "next/dynamic";
 import Quill from "quill";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { toast } from "sonner";
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 interface ChatInputProps{
@@ -17,23 +18,35 @@ interface onSubmitFormprops{
 }
 const ChatInput = ({placeholder}:ChatInputProps) => {
   const editorRef = useRef<Quill | null>(null);
+  const [isPending,setIsPending] = useState(false);
   const workspaceId = useWorkspaceId();
+  const[editorKey, setEditorKey] = useState(0)
   const channelId = useChannelId()
   const {mutate:createMessage} =useCreateMessage()
-    const handleSubmit =({body,file}:onSubmitFormprops) =>{
+    const handleSubmit = async({body,file}:onSubmitFormprops) =>{
         console.log({body, file});
-        createMessage({
+       try {
+        setIsPending(true)
+        await  createMessage({
           body,
           workspaceId,
           channelId
-        })
-        
+        },{throwError:true})
+        setEditorKey((prev) => prev +1)
+       } catch (error) {
+        toast.error("Failed to send message")
+       }
+       finally{
+        setIsPending(false)
+       }
     }
   return (
     <div className="px-5 w-full">
-      <Editor placeholder={placeholder}
+      <Editor 
+      key={editorKey}
+      placeholder={placeholder}
        onSubmit={handleSubmit}
-       disabled={false}
+       disabled={isPending}
        innerRef={editorRef}
 
       />
