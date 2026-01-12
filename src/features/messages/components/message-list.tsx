0bd0f8ba909f969cf2d04e49e326870a570e1832
Jahangeer Ansari <jahangeeransari194@gmail.com>
2026-01-12 15:22:50 +0530

    the end of project with adding reactions

--- src/features/messages/components/message-list.tsx
+++ src/features/messages/components/message-list.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { Id } from "../../../../convex/_generated/dataModel";
 import { useGetCurrentMember } from "@/features/members/api/use-get-current-member";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { Loader } from "lucide-react";
 
 interface MessageListProps {
     channelName?: string;
@@ -86,6 +87,31 @@ const MessageList = ({ canLoadMore, data, isLoadingMore, loadMore, channelCreati
                })}
             </div>
          ))}
+         {/* Adding observer to load message */}
+         <div className="h-1"
+           ref={(ele) =>{
+             if(ele){
+                const observer = new IntersectionObserver(
+                    ([entry]) =>{
+                        if(entry.isIntersecting && canLoadMore){
+                            loadMore()
+                        }
+                    },{threshold:1.0}
+                );
+                observer.observe(ele);
+                  return () => observer.disconnect();
+             }
+           }} 
+         />
+         {isLoadingMore && (
+            <div className="text-center my-2 relative">
+                    <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300"/>
+                    <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
+                
+                     <Loader/>
+                    </span>
+                </div>
+         )}
          {
             variant ==="channel" && channelName && channelCreationTime && (
                 <ChannelHero name={channelName} creationTime={channelCreationTime}/>
